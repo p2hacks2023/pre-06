129722d6d86ec956703e5af184a789d74c574e2b
jugesuke <103935395+jugesuke@users.noreply.github.com>
2023-12-10 14:03:49 +0900

    image をキャプチャしてbase64とるやつ

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState, useCallback } from "react";
+import Webcam from "react-webcam";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    console.log(imageSrc?.toString());
+  }, [webcamRef]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Webcam
+        audio={false}
+        width={540}
+        height={360}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <video id="canvas"></video>
+      <button id="capture" onClick={capture}>
+        キャプチャ
+      </button>
+      <canvas id="draw"></canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
