62a2fcd4995841d77bb8197fc038cab97f802a5e
jugesuke <103935395+jugesuke@users.noreply.github.com>
2023-12-10 16:32:45 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [touchPoint, setTouchPoint] = useState([0, 0]);
+
+  const appRef = useRef<HTMLDivElement>();
+
   const touchMoveHandler = (e: TouchEvent) => {
     e.preventDefault();
 
@@ -13,17 +16,18 @@ function App() {
   };
 
   useEffect(() => {
-    window.addEventListener("touchmove", touchMoveHandler, {
+    appRef.current?.addEventListener("touchmove", touchMoveHandler, {
       passive: false,
     });
   });
 
   return (
     <>
-      <div id="box"></div>
       <span id="zahyo">
         ({touchPoint[0]}, {touchPoint[1]})
       </span>
+      <div id="box" ref={appRef}></div>
+      <div id="filler" ref={appRef}></div>
     </>
   );
 }
