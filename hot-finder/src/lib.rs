df42c80195802f0967b34d7e8bf0bf387f785c2e
Yourein <rein.you.3.1@gmail.com>
2023-12-11 19:14:39 +0900

    Move the implementations to the other file

--- hot-finder/src/lib.rs
+++ hot-finder/src/lib.rs
@@ -1,4 +1,5 @@
 mod utils;
+pub mod mock;
 
 use wasm_bindgen::prelude::*;
 
@@ -13,32 +14,5 @@ pub fn evaluate_hotness(img: String) -> f64 {
 /// The output of this function is base64 encoded PNG image.
 #[wasm_bindgen]
 pub fn extract_hot_buffer(img: String) -> String {
-    img
-}
-
-/// Mock of "evaluate_hotness()"
-/// Output is test_value. In other words, you can decide the exact return value for your debugging.
-/// For negative input, bigger input than 100.0, and other input that couldn't be parsed to f64, this function returns 0.0
-#[wasm_bindgen]
-pub fn evaluate_hotness_mock(test_value: String) -> f64 {
-    match test_value.parse::<f64>() {
-        Ok(v) => {
-            if 0.0 <= v && v <= 100.0 {
-                v
-            }
-            else {
-                0.0f64
-            }
-        },
-        Err(_) => {
-            0.0f64
-        }
-    }
-}
-
-/// Mock of "extract_hot_buffer()"
-/// Output is a static PNG image that has transparent pixels.
-#[wasm_bindgen]
-pub fn extract_hot_buffer_mock(img: String) -> String {
     img
 }
